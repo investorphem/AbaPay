e6e746ad374a09bf46baaf9dfbb47f869ed89b9b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 17:46:25 +0100

    Update discos.ts

--- src/app/discos.ts
+++ src/app/discos.ts
@@ -8,7 +8,7 @@ export const ELECTRICITY_DISCOS = [
     { serviceID: "ikeja-electric", displayName: "Ikeja Electricity", logo: "/ikeja.png" },
     { serviceID: "abuja-electric", displayName: "Abuja Electricity", logo: "/abuja.png" }
     { serviceID: "eko-electric", displayName: "Eko Eletricity", logo: "/eko.png" 
-    { serviceID: "enugu-electric", displayName: "Enugu Electricity", logo: "/enugu.png" },
+    { serviceID: "enuu-electric", displayName: "Enugu Electricity", logo: "/enugu.png" },
     { serviceID: "kano-electric", displayName: "Kano Electrcity", logo: "/kano.png" 
     { serviceD: benin-electric", displayName: "Benin lectiity, logo: "/benin.pn
     { serviceID: "yola-electric", displayName: "Yola Electriity", logo: "/yola.png" }, // Added from imag
