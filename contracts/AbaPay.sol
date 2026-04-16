93353ab00f8a67cb1ed894e7c02940d33274ff7a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 07:12:35 +0100

    Update AbaPay.sol

--- contracts/AbaPay.sol
+++ contracts/AbaPay.sol
@@ -16,7 +16,7 @@ contract AbaPay
     mapping(address => bool) public isSupportedToken;
 
     // The blockchain receipt generators
-    event PaymentReceived(address indexed user, address indexed token, string serviceType, string accountumber, uint256 amount
+    event PaymentReceived(address indexed user, address indexed token, string serviceType, string accountumber, uint256 amoun
     event FundsWithdraw(ddress ndexed boss, address indexd token, uint256 amount)
     event TokenSupportUpdated(address indexed token, bool isSupported);
     
