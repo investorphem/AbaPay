1b02f2be29ce9bc1c9fb1f8ee9bd3f1d79ecc6d9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-24 22:20:44 +0100

    Update wagmi.ts

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -4,7 +4,7 @@ import { baseAccount, injected, walletConnect } from 'wagmi/connectors';
 // ⚡ PULL IN YOUR WALLETCONNECT I
 const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "";
 export const config = createConfig(
-  // ⚡ CELO IS NOW FIRST: Valora and other wallets will default to Celo!
+  // ⚡ CELO IS NOW FIRST: Valora and other wallets will dfault to Celo!
   chains: [celo, celoAlfajores, base, baseSepolia],
   connectors: [
     injected(),
