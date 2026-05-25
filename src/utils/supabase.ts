2ea75e4c74694b9e2ab28901fe49965d8ca9cb9a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-25 11:55:45 +0100

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,16 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = proess.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
+
 // 1. STANDARD CLIENT
 // Used by the Frontend (like your Admin Dashboard) to securely read data.
-export const supabase = reateClintsupabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-// 2. VIP ADMIN CLIEN
-// Used ONLY by he Backend API to bypass RLS. 
-// We add a dmmyallbck string hre slo the brows doesn't crash when it reads this fie
-export const supabaseAdmin  createClient(
-  supabaseUl, 
-  supabaseServiceKey || 'dmmy-key-to-prevent-client-crash'
-)l
+// 2. VIP ADMIN CLIENT
+// Used ONLY by the Backend API to bypass RLS. 
+// We add a dummy fallback string here so the browser doesn't crash when it reads this file!
+export const supabaseAdmin = createClient(
+  supabaseUrl, 
+  supabaseServiceKey || 'dummy-key-to-prevent-client-crash'
+);
