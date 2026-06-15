3346233405cf91ffe31c09f932e16b992f82382e
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-15 14:59:02 +0100

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,7 +6,7 @@ export const ABAPAY_ABI = [{"inputs":[{"internalType":"address","name":"tokenAdd
 // ⚡ UPDATED: Added the 'allowance' function so Viem can check permissions
 export const ERC20_ABI = [
   {"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},
-  {"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+  {"inputs":[{"internalType":"address","name":"account","type":"address"}],
   {"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance"
 
 export const SERVICES = [
