393ea14015ffdd91ab7028b382d068ce40129cbc
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-15 15:01:24 +0100

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -6,9 +6,7 @@ const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
 
 // 1. STANDARD CLIENT
 // Used by the Frontend (like your Admin Dashboard) to securely read data.
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-// 2. VIP ADMIN CLIENT
+export const supabase = createClient(supabaseUrl, supabase
 // Used ONLY by the Backend API to bypass RLS. 
 // We add a dummy fallback string here so the browser doesn't crash when it reads this file!
 export const supabaseAdmin = createClient(
