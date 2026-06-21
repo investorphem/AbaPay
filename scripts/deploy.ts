d8602a96007d0461ebe997366246cbcfae41570b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-21 17:46:49 +0100

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -20,7 +20,6 @@ async function main() {
   if (networkName === "celo") {
     // MAINNET ADDRESSES
     usdt = "0x48065fbbe25f71c9282ddf5e1cd6d6a887483d5e";
-    usdc = "0xcebA9300f2b948710d2653dD7B07f33A8B32118C";
     // SEPOLIA TESTNET ADDRESSES
     usdt = "0xd077A400968890Eacc75cdc901F0356c943e4fDb";
   let tx2 = await AbaPay.setTokenSupport(usdc, true);
