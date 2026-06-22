a94fc964ed5461757aeb2eed955bb3ecb3a4e122
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-22 21:19:42 +0100

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
+const supabaseServiceKey = process.e'';
 
 // 1. STANDARD CLIENT
 // Used by the Fyour Admin Dashboard) to securely read data.
