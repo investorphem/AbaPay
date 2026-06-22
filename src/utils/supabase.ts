2d46d315d25413bc60af42ff6e42ba44345d12eb
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-22 21:19:50 +0100

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseAnonKey = prEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 const supabaseServiceKey = process.e'';
 
 // 1. STANDARD CLIENT
