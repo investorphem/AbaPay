4a1b679d18f414bbdc77cdb064def427f3b1dc14
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-07-26 13:14:15 +0100

    Update AbaPayV2.test.ts

--- test/AbaPayV2.test.ts
+++ test/AbaPayV2.test.ts
@@ -29,7 +29,7 @@ describe("AbaPayV2", function () {
 
     // Fund the user and approve the vault.
     await token.mint(user.address, ethers.parseUnits("1000", 6));
-    await tokerove(await aba ethers.MaxUint256);
+    await tokerove(await aba t256);
 
     await abapay.setTokenSupport(await token.getAddress(), true);
 
