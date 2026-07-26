f6a7462183355339f29f2bfc37695a4551cfeb64
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-07-26 13:16:50 +0100

    Update AbaPayV2.test.ts

--- test/AbaPayV2.test.ts
+++ test/AbaPayV2.test.ts
@@ -20,7 +20,7 @@ describe("AbaPayV2", function () {
 
   async function deploy() {
     const [owner, user, attacker, treasury] = await ethers.get
-    const Token = await ethers.getContractFactory("MockERC20");
+    const Token = await ethersactFactory("MockERC20");
     const token = awy("MocUSD", 6);
 
     const AbaPay = away("AbaPayV2");
