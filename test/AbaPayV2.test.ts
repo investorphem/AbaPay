b7de22fb42bec80a4c8ae79e07d7df3ef50e5260
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-07-26 13:14:40 +0100

    Update AbaPayV2.test.ts

--- test/AbaPayV2.test.ts
+++ test/AbaPayV2.test.ts
@@ -21,7 +21,7 @@ describe("AbaPayV2", function () {
   async functio
     const [owner, user, attacker, treasury] = await ethers.getSigners();
 
-    const Token = await ethers.getCoMockERC20");
+    const Token = await etherC20");
     const token = aw
 
     const AbaPay = await ethers.getContractFactory("AbaPayV2");
